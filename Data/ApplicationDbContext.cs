cf8e229e74d9b993784861948741f4d7c5240b95
Julio <juliobrsouza@outlook.com>
2026-07-16 14:56:22 -0300

    BREAKING CHANGE: moved Sonata Desktop to the same repository as the API, and changed both namespaces

--- Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace sonata_api.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<Models.Session> Sessions => Set<Models.Session>();
-    public DbSet<Models.Message> Messages => Set<Models.Message>();
-}
